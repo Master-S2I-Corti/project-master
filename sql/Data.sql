090b90b6c845024e10038498524ac8516df9197e
Martinet Etienne <e.martinet0@gmail.com>
2018-03-02 14:54:15 +0100

    Edit data.sql

--- sql/Data.sql
+++ sql/Data.sql
@@ -14,9 +14,9 @@ ALTER TABLE `Personne` CHANGE `created_at` `created_at` TIMESTAMP NOT NULL DEFAU
 
 
 INSERT INTO `Personne` (`id`, `login`, `password`, `nom`, `prenom`, `tel`, `email`, `code_postal`, `ville`, `adresse`, `remember_token`, `created_at`, `updated_at`, `admin`, `code_professeur`, `code_etudiant`) VALUES
-  (1, 'etu', '$2y$10$YUV3bcd.JwBk/ci29kjXhucPfeQV1NC48GSdRE0xbk8e5LG1FWWCW', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ZMJx9PiZSOeEsU6sWBdyOpyTPtJ0gA5bRRXHu5kXElT4qVxHMwB6utplZyK3', '2018-01-28 16:00:17', '2018-01-28 16:00:17', 0, NULL, 1),
+  (1, 'etu', '$2y$10$YUV3bcd.JwBk/ci29kjXhucPfeQV1NC48GSdRE0xbk8e5LG1FWWCW', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ZMJx9PiZSOeEsU6sWBdyOpyTPtJ0gA5bRRXHu5kXElT4qVxHMwB6utplZyK3', '2018-01-28 16:00:17', '2018-01-28 16:00:17', 0, NULL, NULL),
   (2, 'admin', '$2y$10$YUV3bcd.JwBk/ci29kjXhucPfeQV1NC48GSdRE0xbk8e5LG1FWWCW', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'rwVdvEgmC6jvkA5ku7DiRXC7GmuPOwSZ6J4tfZZVFT8OLoau1n8cQa05zMAl', '2018-01-28 15:01:07', '2018-01-28 15:01:07', 1, NULL, NULL),
-  (3, 'ens', '$2y$10$YUV3bcd.JwBk/ci29kjXhucPfeQV1NC48GSdRE0xbk8e5LG1FWWCW', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-01-28 16:03:17', '2018-01-28 16:03:17', 0, 1, NULL);
+  (3, 'ens', '$2y$10$YUV3bcd.JwBk/ci29kjXhucPfeQV1NC48GSdRE0xbk8e5LG1FWWCW', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-01-28 16:03:17', '2018-01-28 16:03:17', 0, NULL, NULL);
 
 
 INSERT INTO `Enseignant` (`code_professeur`, `id`) VALUES
@@ -25,6 +25,9 @@ INSERT INTO `Enseignant` (`code_professeur`, `id`) VALUES
 INSERT INTO `Etudiant` (`code_etudiant`, `id_annee`, `id`) VALUES
 (1, NULL, 1);
 
+
+UPDATE Personne SET code_etudiant = 1 WHERE id = 1;
+UPDATE Personne SET code_professeur = 1 WHERE id = 2;
 
 
 INSERT INTO UFR (libelle) VALUE ('Science et technique');
