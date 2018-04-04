df088de14224c5706b070021436e777072885b5f
Martinet Etienne <e.martinet0@gmail.com>
2018-04-04 18:39:52 +0200

    edt dyna

---
+++ app/Seance.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Seance extends Model
+{
+
+    protected $table = "Seance";
+
+    public $timestamps = false;
+
+    public function matiere() {
+        return $this->hasOne('App\Matiere', "id_matiere", "id_matiere");
+    }
+
+
+    public function getEcart() {
+        $h1= date('H', strtotime($this->heure_debut));
+        $h2= date('H',strtotime( $this->heure_fin));
+        $m1= date('M',strtotime( $this->heure_debut));
+        $m2= date('M',strtotime($this->heure_fin));
+
+        return ($h2-$h1)*4+($m2-$m1)/15;
+
+    }
+
+}
+
--- resources/views/edtAdmin.blade.php
+++ resources/views/edtAdmin.blade.php
@@ -1,14 +1,38 @@
 
 
-
+<?php
+use App\Http\Controllers\EDTController;
+?>
 @extends('layouts.app')
 
 @section('css')
 <style>
-    .custom-select {border: 1px solid blue; color: blue}
-    .custom-select:focus {color: blue}
-    td, th {border: 1px solid black;}
+    td, th {border: 1px solid rgba(0, 0, 0, 0.52);}
     table {border-collapse : collapse;}
+    .heure, .minute {
+        text-align: end;
+        vertical-align: middle;
+    }
+    .minute {
+        font-size: 12px;
+        color:gray;
+        font-style: italic;
+    }
+    .tr-inner {
+        border-bottom: 0px;
+    }
+    tr:first-child td {
+        border: 1px solid rgba(0, 0, 0, 0.52);
+        padding:.7rem;
+
+    }
+    tr:last-child td {
+        border-bottom: 1px solid rgba(0, 0, 0, 0.52);
+    }
+    .table td {
+        padding:.2rem;
+    }
+
 </style>
 @endsection
 @section('content')
@@ -49,343 +73,39 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th></th>
-                            <th>Lundi</th>
-                            <th>Mardi</th>
-                            <th>Mercredi</th>
-                            <th>Jeudi</th>
-                            <th>Vendredi</th>
-                            <th>Samedi</th>
-                        </tr>
-                        </thead>
+                    <table class="table" id="table_edt">
                         <tbody>
                         <tr>
-                            <td>8h-8h15</td>
                             <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            @for ($j = 8; $j < 22; $j++)
+                                <td class="heure" colspan="2">{{$j}}:00 -</td>
+                                <td class="minute" colspan="2">{{$j}}:30</td>
+                            @endfor
                         </tr>
-                        <tr>
-                            <td>8h15-8h30h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>8h30-8h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>8h45-9h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>9h-9h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>9h15-9h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>9h30-9h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>9h45-10h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>10h-10h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>10h15-10h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>10h30-10h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>10h45-11h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>11h-11h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>11h15-11h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>11h45-12h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>12h-13h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>13h-13h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>13h15-13h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>13h30-13h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>13h45-14h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>14h-14h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>14h15-14h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>14h30-14h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>14h45-15h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>15h-15h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>15h15-15h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>15h30-15h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>15h45-16h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>16h-16h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>16h15-16h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>16h30-16h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>16h45-17h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>17h-17h15</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>17h15-17h30</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>17h30-17h45</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>17h45-18h</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                        @for ($i = 0; $i < 7; $i++)
+                            <tr>
+                                <td style="text-align: end">{{strftime("%a %d %b", strtotime($date . ' +'.$i.' day'))}}</td>
+                                @for ($j = 8; $j < 22; $j++)
+                                        @for ($k = 0; $k < 4; $k++)
+
+                                            <?php
+                                                $seance = EDTController::getHoraire($seances, strtotime($date . ' +'.$i.' day'), $j, 15*$k);
+                                                if($seance != null) {
+                                                    $ecart = $seance->getEcart();
+                                                    echo "<td style='background:red' colspan='".$ecart."' class=\"tr-inner\" id=\"table{{$i}}{{$j}}\">".$seance->matiere->libelle."</td>";
+                                                    $k = $k+$ecart;
+
+                                                } else {
+                                                    echo "<td class=\"tr-inner\" id=\"table{{$i}}{{$j}}\"></td>";
+
+                                                }
+                                            ?>
+                                        @endfor
+                                @endfor
+                            </tr>
+                        @endfor
+
+
                         </tbody>
                     </table>
                 </div>
@@ -405,6 +125,97 @@
         </div>
     </div>
 
+    <script>
 
+        var copyStyle = function(source, dest)
+        {
+            dest.style.cssText = source.style.cssText;
+            dest.className = source.className;
+            dest.id = source.id;
+        }
+
+        var reverse_table = function(thetable)
+        {
+            var rtable = thetable.cloneNode(false);
+            var rtable_body = rtable.appendChild(document.createElement("tbody"));
+
+            // On compte les colonnes
+            var tableNumCols = 0;
+            for(var tdi = 0; tdi < thetable.rows[0].cells.length; tdi++)
+            {
+                tableNumCols += thetable.rows[0].cells[tdi].colSpan;
+            }
+
+            // On compte les lignes
+            var tableNumRows = 0;
+            for(var tri = 0; tri < thetable.rows.length; tri++)
+            {
+                tableNumRows += thetable.rows[tri].cells[0].rowSpan;
+            }
+
+            // on crée les tr dont on a besoin
+            var rtable_trs = Array();
+            var rowSpanDuration = Array();
+            var rowSpanValue = Array();
+            var cols = thetable.getElementsByTagName("col");
+            for(var tri = 0; tri < tableNumCols; tri++)
+            {
+                var current_tr = document.createElement("tr");
+                if(tri < cols.length)
+                {
+                    copyStyle(cols[tri], current_tr);
+                }
+                rtable_trs[tri] = current_tr;
+                rtable_body.appendChild(current_tr);
+                rowSpanDuration[tri] = 1;
+                rowSpanValue[tri] = 1;
+            }
+
+            // On va retourner le tableau maintenant
+            var celli = 0;
+            for(var tri = 0; tri < thetable.rows.length; tri++)
+            {
+                var col = document.createElement("col");
+
+                copyStyle(thetable.rows[tri], col);
+                rtable.appendChild(col);
+
+                for(var tdi = 0; tdi < thetable.rows[tri].cells.length; tdi++)
+                {
+                    var cell = thetable.rows[tri].cells[tdi].cloneNode(true);
+
+                    // Calcul de la position d'insertion
+                    var rowOfInsertion = celli % tableNumCols;
+
+                    // Gestion des row span
+                    var rsdi = rowOfInsertion;
+
+                    while(rowSpanDuration[rsdi] > 1)
+                    {
+                        celli += rowSpanValue[rsdi];
+                        rowOfInsertion = celli % tableNumCols; // maj de la position d'insertion
+                        rowSpanDuration[rsdi]--;
+                        rsdi = rowOfInsertion;
+                    }
+                    rowSpanDuration[rsdi] += cell.rowSpan - 1;
+                    rowSpanValue[rsdi] = cell.colSpan;
+
+                    // échanger rowSpan and colSpan
+                    var colSpan = cell.colSpan;
+                    cell.colSpan = cell.rowSpan;
+                    cell.rowSpan = colSpan;
+
+                    // Insertion de la cellule
+                    rtable_trs[rowOfInsertion].appendChild(cell);
+
+                    // Gestion des col span
+                    celli += colSpan; // colSpan vaut 1 pour une cellule classique
+                }
+            }
+
+            thetable.parentNode.replaceChild(rtable, thetable);
+        }
+        reverse_table(document.getElementById("table_edt"));
+    </script>
     @include("edtAjout")
 @endsection
