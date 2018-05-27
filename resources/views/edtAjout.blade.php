8012ca320e81c4a9b50e493549a66a4017172bb0
Damien2b <damien.petetin@gmail.com>
2018-05-27 22:39:42 +0200

    modification page ajout 1

--- resources/views/edtAjout.blade.php
+++ resources/views/edtAjout.blade.php
@@ -90,7 +90,8 @@
                             </div>
                             <div class="col-md-6">
                                     <input required type="time" name="heure_debut" class="form-control"
-                                           placeholder="Heure début ex: 08h00">
+                                           placeholder="Heure début ex: 08h00"
+                                           min="08:00" max="21:30">
                             </div>
                         </div>
                         <div class="row">
@@ -99,7 +100,8 @@
                             </div>
                             <div class="col-md-6">
                                     <input required type="time" name="heure_fin" class="form-control"
-                                           placeholder="Heure fin ex: 18h00">
+                                           placeholder="Heure fin ex: 18h00"
+                                           min="08:00" max="21:30">
                             </div>
                         </div>
                         <div class="row">
@@ -108,7 +110,7 @@
                             </div>
                             <div class="col-md-6">
                                 <div id=dateDuplicate class="btn-group">
-                                    <input name="date"  class= "form-control" id="date" type="date">
+                                    <input required="" name="date"  class= "form-control" id="date" type="date">
                                     
                                 </div>
                                 <div class="row">
