5835133f48ca851f3dca1a11c7d6a68e1c75024c
Aminesefacene <sef.amine@hotmail.fr>
2018-05-27 16:31:43 +0200

    modif

---
+++ app/Http/Controllers/FiliereController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Diplome;
+use App\UFR;
+use App\Annee;
+
+class FiliereController extends Controller
+{
+    public function vueAdmin()
+    {
+        $filieres = Diplome::get();
+        $ufr = UFR::get();
+        $annees = Annee::get();
+        return view('filiere',compact('filieres','ufr','annees'));
+    }
+
+    public function vueEnseignant()
+    {
+        $filieres = Diplome::get();
+        $ufr = UFR::get();
+        $annees = Annee::get();
+        return view('filiereEns',compact('filieres','ufr','annees'));
+    }
+}
